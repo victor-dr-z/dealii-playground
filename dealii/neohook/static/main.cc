40a7d445ef6709abb6907dc014808a128bdac647
W <wzheng@carbon3d.com>
2018-12-26 12:53:15 -0800

    completed main.cc

---
+++ dealii/neohook/static/main.cc
@@ -0,0 +1,25 @@
+
+#include "driver.h"
+
+int main(int argc, char * argv[]) {
+  if (argc!=2) {
+    std::cerr << "*** Call this program as ./neohook params.prm ***"
+    return 1;
+  }
+  params::DeclareParams();
+  params::GlobPrm.parse_input(argv[1]);
+  dealii::Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv,
+      dealii::numbers::invalid_unsigned_int);
+  auto dim = params::GetParam("dimension");
+  switch (dim) {
+    case 3: {
+      Driver<3> problem;
+      problem.run();
+      break;
+    }
+    default: {
+      std::cerr << "only 3d is implemented";
+      return 1;
+    }
+  }
+}
